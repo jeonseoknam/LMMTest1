00c5705c8e7f93d74c6e5cc8d73f9402ceb575f8
Jeonseoknam <sukkzz7065se@gmail.com>
2024-11-10 17:46:46 +0900

    test1

---
+++ app/src/main/java/com/foo/lmm_test1/MainActivity.java
@@ -0,0 +1,24 @@
+package com.foo.lmm_test1;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.foo.lmm_test1.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+}
